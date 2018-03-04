d2c3efc0bc2e2f2c48a258d8d974ba7f47153a0b
Jonathan Vargas <jonathanv3232@gmail.com>
2018-03-03 23:59:34 -0500

    returns a node list based on the selector

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 (function(global) {
-  function QueryLite() {
-    return new QueryLite.init();
+  function QueryLite(selector) {
+    return new QueryLite.init(selector);
   }
 
   QueryLite.prototype = {};
 
-  QueryLite.init = function() {
+  QueryLite.init = function(selector) {
     this.version = '0.1.0';
+    //handles no selector
+    if (!selector) {
+      // even without a selector it is still chainable
+      return this;
+    } else {
+      const elem = document.querySelectorAll(selector);
+      console.log(elem);
+      return elem;
+    }
   };
 
   QueryLite.init.prototype = QueryLite.prototype;
